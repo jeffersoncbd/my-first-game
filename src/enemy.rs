740f6cf0b764a99e55992cf25d7799b5bf226bdb
Jefferson Carlos <jeff.carlosbd@gmail.com>
2023-02-28 15:12:59 -0300

    implementa o primeiro movimento dos inimigos (eliptico)

--- src/enemy.rs
+++ src/enemy.rs
@@ -5,7 +5,8 @@ use rand::{thread_rng, Rng};
 
 use crate::{
     components::{Enemy, FromEnemy, Laser, Movable, SpriteSize, Velocity},
-    EnemyCount, GameTextures, WinSize, ENEMY_LASER_SIZE, ENEMY_MAX, ENEMY_SIZE, SPRITES_SCALE,
+    EnemyCount, GameTextures, WinSize, BASE_SPEED, ENEMY_LASER_SIZE, ENEMY_MAX, ENEMY_SIZE,
+    SPRITES_SCALE, TIME_STEP,
 };
 
 pub struct EnemyPlugin;
@@ -20,7 +21,8 @@ impl Plugin for EnemyPlugin {
             SystemSet::new()
                 .with_run_criteria(enemy_fire_criteria)
                 .with_system(enemy_fire_system),
-        );
+        )
+        .add_system(enemy_moviment_system);
     }
 }
 
@@ -88,5 +90,47 @@ fn enemy_fire_system(
             .insert(FromEnemy)
             .insert(Movable { auto_despawn: true })
             .insert(Velocity { x: 0., y: -1. });
+    }
+}
+
+fn enemy_moviment_system(time: Res<Time>, mut query: Query<&mut Transform, With<Enemy>>) {
+    let now = time.elapsed_seconds_f64() as f32;
+    for mut transform in query.iter_mut() {
+        // current position
+        let (x_org, y_org) = (transform.translation.x, transform.translation.y);
+
+        // max distance
+        let max_distance = TIME_STEP * BASE_SPEED;
+
+        // fixtures
+        let dir: f32 = -1.;
+        let (x_pivot, y_pivot) = (0., 0.);
+        let (x_radius, y_radius) = (200., 130.);
+
+        // compute next angle
+        let angle = dir * BASE_SPEED * TIME_STEP * now % 360. / PI;
+
+        // compute target x/y
+        let x_dst = x_radius * angle.cos() + x_pivot;
+        let y_dst = y_radius * angle.sin() + y_pivot;
+
+        // compute distance
+        let dx = x_org - x_dst;
+        let dy = y_org - y_dst;
+        let distance = (dx * dx + dy * dy).sqrt();
+        let distance_ratio = if distance != 0. {
+            max_distance / distance
+        } else {
+            0.
+        };
+
+        // compute final x/y
+        let x = x_org - dx * distance_ratio;
+        let x = if dx > 0. { x.max(x_dst) } else { x.min(x_dst) };
+        let y = y_org - dy * distance_ratio;
+        let y = if dy > 0. { y.max(y_dst) } else { y.min(y_dst) };
+
+        let translation = &mut transform.translation;
+        (translation.x, translation.y) = (x, y);
     }
 }
